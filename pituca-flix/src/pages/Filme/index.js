33332502f67d1297d166c841356ac57f97783c47
WillianRRP <willianrenato754@gmail.com>
2025-03-08 20:30:15 -0300

    movie details pt1

--- pituca-flix/src/pages/Filme/index.js
+++ pituca-flix/src/pages/Filme/index.js
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+
 function Filme(){
+    const{ id } = useParams()
+    const[filme, setFilme] = useState({})
+    const[Loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`, {
+                params:{
+                    api_key:"09cb032a5dc82af31448350df5b23055",
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false)
+            })
+            .catch(()=>{
+                console.log("O filme não foi encontrado");
+            })
+        }
+
+        loadFilme();
+
+        return () => {
+            console.log("Componente foi desmontado");
+        }
+    }, [])
+
+    if(Loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes...</h1>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>Bem vindo a pagina detalhes de filmes</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} /10</strong>
         </div>
     )
 }
